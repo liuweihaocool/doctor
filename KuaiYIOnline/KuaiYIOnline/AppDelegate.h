f1c529b128eafb5c687c4725a6b121e134500113
liuweihaocool <414716652@qq.com>
2016-03-11 13:50:05 +0800

    初始化项目

---
+++ KuaiYIOnline/KuaiYIOnline/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KuaiYIOnline
+//
+//  Created by LiuWei on 3/11/16.
+//  Copyright © 2016 Doctor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KuaiYIOnline/KuaiYIOnline/Classes/Menu/Controller/LWBasicMenuController.h
@@ -0,0 +1,13 @@
+//
+//  LWBasicMenuController.h
+//  KuaiYIOnline
+//
+//  Created by LiuWei on 3/11/16.
+//  Copyright © 2016 Doctor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LWBasicMenuController : UITableViewController
+
+@end
